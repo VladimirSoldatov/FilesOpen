09adfdc02b9ce75e7a4f60cff78e3f6fff6bf7c8
Владимир Солдатов <cbb110@rambler.ru>
2022-06-16 14:52:56 +0300

    Text

--- FilesOpen/FilesOpen.cpp
+++ FilesOpen/FilesOpen.cpp
@@ -9,36 +9,26 @@ int main()
     int size = 10;
     char arr[80];
     char arr2[80];
+    cout << "Enter text and press Enter\n";
     cin >> arr;
 
     if ((fopen_s(&file, path, "w")) != NULL)
         cout << "The file could not be created\n";
     else
         fprintf(file, "%s", arr);
-    if (fclose(file) == EOF)
-        cout << "The file is not closed";
+    if (fclose(file) == EOF || fclose(file) == 0)
+        cout << "The file is not closed\n";
     else
-        cout << "The file is closed";
-
+        cout << "The file is closed\n";
+    fflush(file);
     if (fopen_s(&file, path, "r") != NULL)
-        cout << "The file cannot be opened";
+        cout << "The file cannot be opened\n";
     else
 
     {
-
-        while (!feof(file))
-        {
-
-            fscanf_s(file, "%s", arr2, sizeof(arr2));
-
-            cout << arr2 <<endl;
-
-        }
-
-
-
+        fscanf_s(file, "%s", arr2, (unsigned int)sizeof(arr2));
+        cout << arr2 << endl;
+        cout << "Text has been read";
     }
-
-
-
+    fflush(file);
 }

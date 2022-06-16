d919bfa02b5ce1793a48ee22b14d163a25c95418
Ефимов Максим <efimov_m@itstep.academy>
2022-06-16 21:23:04 +0300

    Чтение

--- FilesOpen/FilesOpen.cpp
+++ FilesOpen/FilesOpen.cpp
@@ -4,31 +4,35 @@
 using namespace std;
 int main()
 {
-    FILE* file = NULL;
-    char path[] = "text.txt";
-    int size = 10;
-    char arr[80];
-    char arr2[80];
-    cout << "Enter text and press Enter\n";
-    cin >> arr;
+	const char* path = "D:\\1.txt"; 
+	const char* path2 = "D:\\2.txt";
 
-    if ((fopen_s(&file, path, "w")) != NULL)
-        cout << "The file could not be created\n";
-    else
-        fprintf(file, "%s", arr);
-    if (fclose(file) == EOF || fclose(file) == 0)
-        cout << "The file is not closed\n";
-    else
-        cout << "The file is closed\n";
-    fflush(file);
-    if (fopen_s(&file, path, "r") != NULL)
-        cout << "The file cannot be opened\n";
-    else
 
-    {
-        fscanf_s(file, "%s", arr2, (unsigned int)sizeof(arr2));
-        cout << arr2 << endl;
-        cout << "Text has been read";
-    }
-    fflush(file);
+	int len = 0;
+	char buf[128];
+	FILE* in;
+	FILE* out{};
+	
+	if (fopen_s(&out, path2, "w") != NULL && &in, fopen_s(&in, path, "r") != NULL)
+	{
+
+		cout << "The files cannot be opened";
+	}
+	else
+	{
+		while (!feof(in)) {
+			fgets(buf, 127, in);
+			len = (int)strlen(buf);
+		/*	if (buf[len - 1] == '\n')
+			{
+				buf[len - 1] = '\0';
+			}
+			*/
+			if (len < 7) 
+			{
+				fputs(buf, out);
+				puts(buf);
+			}
+		}
+	}
 }
